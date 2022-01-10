64988f8ffcc2c753266ba9672a3cd9900de7e248
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-01-10 21:50:48 +0100

    feta: smoke tests

--- python/test_smoke.py
+++ python/test_smoke.py
@@ -1,7 +1,17 @@
 import time
+import urllib.request
 
-
-def test_basic():
-    time.sleep(5)
+   
+def test_default():
+    req = urllib.request.Request('http://localhost:8080/hello')
+    with urllib.request.urlopen(req) as response:
+       the_page = response.read().decode("utf8")
+    assert the_page.find("Hello World!") != -1
     pass
 
+def test_name():
+    req = urllib.request.Request('http://localhost:8080/hello?name=foo')
+    with urllib.request.urlopen(req) as response:
+       the_page = response.read().decode("utf8")
+    assert the_page.find("Hello bar!") != -1
+    pass
