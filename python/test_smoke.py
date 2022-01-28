df5d74d0a139c82c68a0930fe81339b9f71ef626
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-01-28 15:08:09 +0000

    add slowness test

--- python/test_smoke.py
+++ python/test_smoke.py
@@ -5,8 +5,8 @@ from random import randint
 from time import sleep
 
 
-def test_default():
-    sleep(randint(400,1000))
+def test_healthcheck():
+    sleep(randint(1,2))
     url = os.getenv("SMOKE_TEST_URL")
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
@@ -14,8 +14,18 @@ def test_default():
     assert the_page.find("Hello World!") != -1
     pass
 
-def test_name():
-    sleep(randint(100,2000))
+def test_healthcheck_with_request():
+    sleep(randint(1,2))
+    url = os.getenv("SMOKE_TEST_URL")
+    req = urllib.request.Request("{}?name=foo".format(url))
+    with urllib.request.urlopen(req) as response:
+       the_page = response.read().decode("utf8")
+    assert the_page.find("Hello foo!") != -1
+    pass
+
+# For adding slowness
+def test_multiple_healthchecks():
+    sleep(randint(1,4))
     url = os.getenv("SMOKE_TEST_URL")
     req = urllib.request.Request("{}?name=foo".format(url))
     with urllib.request.urlopen(req) as response:
